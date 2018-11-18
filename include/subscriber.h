9c6222512b68c4d9531f27c090330948d78b71aa
Andy Meyer <itechahm@me.com>
2018-11-17 21:57:16 -0800

    Merge pull request #16 in ALC/extensions from cleanup/old_files to master
    Squashed commit of the following:
    
    commit 2dbdd86739a492b4c50374c379859c6ce3407849
    Author: Andy Meyer <ahm7197@rit.edu>
    Date:   Sun Nov 18 00:56:12 2018 -0500
    
        cleaned up deprecated files and interfaces.

--- include/subscriber.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "debug.h"
-typedef struct subscriber_S proto_subscriber;
-struct subscriber_S     {
-    void (*notify)(void *message);
-    void *self;
-};
-
--- static_dispatch.c
+++
@@ -1,4 +0,0 @@
-typedef struct proto_subscriber_S {
-    void (*notify)(struct proto_subscriber_S *self, int channel, void *message);
-    void *data;
-} proto_subscriber;
