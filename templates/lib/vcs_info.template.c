fa370429166e6681d5d177b2e14e487cc1abf8b6
andy <ahm7197@rit.edu>
2020-06-17 14:34:24 -0700

    Merge pull request #50 in ALC/containers from eng/ALC-69_ALC-75_commit_tags to master
    Squashed commit of the following:
    
    commit 8ce53420ec97eb1b5959dfa5f7f5b95a44d3702b
    Author: Andy Meyer <ahm7197@rit.edu>
    Date:   Wed Jun 17 17:31:53 2020 -0400
    
        Test names are consisent, git hashes now added.
    
        The names of test executables now match their source file names, for
        clarity.
        Git commit hashes are now added to every library as a private symbol,
        with the intention of using them for version-based debugging and bug
        tracking.
        ALC-Debug, whenever that gets built, will support dumping this
        information.

---
+++ templates/lib/vcs_info.template.c
@@ -0,0 +1,6 @@
+/**
+ * This file is a template for Meson to generate the real vcs_info.c with.
+ * The file gets built into each of the shared objects, the symbol will then
+ * be available during debugging and can be included in traces and debug logs.
+ */
+static const char ALC_CONTAINERS_COMMIT_HASH[] = "@VCS_TAG@";
